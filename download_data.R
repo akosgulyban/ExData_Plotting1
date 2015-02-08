45b2350ebf340439eefbd739a9b556a78bc88be6
akosgulyban <akosgulyban@users.noreply.github.com>
2015-02-09 00:58:21 +0100

    Delete download_data.R

--- download_data.R
+++
@@ -1,6 +0,0 @@
-# Downloading and extracting the dataset if not yet performed 
-if (!file.exists("household_power_consumption.txt")){
-    fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-    download.file(fileUrl, destfile = "Dataset.zip")
-    unzip("Dataset.zip")
-}
